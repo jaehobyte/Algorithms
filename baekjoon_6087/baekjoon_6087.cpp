a306996dfbb48ab0817376b01dff14dfa60b3d61
jaehomersimpson <p5564989@gmail.com>
2022-03-06 00:09:43 +0900

    BFS - laser communication
    BFS - laser communication done

--- baekjoon_6087/baekjoon_6087.cpp
+++ baekjoon_6087/baekjoon_6087.cpp
@@ -5,72 +5,64 @@
 #include <algorithm>
 using namespace std;
 
-int W, H;
-char map[100][100];
 int dx[] = { 1, -1, 0, 0 };
 int dy[] = { 0, 0, 1, -1 };
 
-int sx, sy, ex, ey;
-int mirror;
-queue<pair<int, int>> q;
-
-void bfs()
-{
-    vector<vector<int>> visited(H, vector<int>(W, -1));
-    visited[sx][sy] = 0;
-
-    while (!q.empty())
-    {
-        int x, y;
-        tie(x, y) = q.front(); q.pop();
-
-        for (int k = 0; k < 4; k++) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
-            while (0 <= nx && nx < W && 0 <= ny && ny < H) {
-                if (map[nx][ny] == '*') break;
-                if (visited[nx][ny] == -1) {
-                    visited[nx][ny] = visited[x][y] + 1;
-                    q.push(make_pair(nx, ny));
-                }
-                nx += dx[k];
-                ny += dy[k];
-            }
-        } 
-    }
-    cout << visited[ex][ey] << endl;
-}
-
 int main()
 {
-    cin >> W >> H;
+	int W, H;
+	cin >> W >> H;
 
-    sx = sy = ex = ey = -1;
+	int sx, sy, ex, ey;
+	sx = sy = ex = ey = -1;
+	vector<string> map(H);  
 
-    for (int i = 0; i < H; i++)
-    {
-        string temp;
-        cin >> temp;
-        for (int j = 0; j < W; j++)
-        {
-            map[i][j] = temp[j];
-            if (map[i][j] == 'C')
-            {
-                if (sx == -1)
-                {
-                    sx = i;
-                    sy = j;
-                }
-                else
-                {
-                    ex = i;
-                    ex = j;
-                }
-            }
-        }
-    }
+	for (int i = 0; i < H; i++)
+	{
+		cin >> map[i];
+		for (int j = 0;j < W; j++)
+		{
+			if (map[i][j] == 'C')
+			{
+				if (sx == -1)
+				{
+					sx = i;
+					sy = j;
+				}
+				else {
+					ex = i;
+					ey = j;
+				}
+			}
+		}
+	}
 
-    q.push(make_pair(sx, sy));
-    bfs();
+	vector<vector<int>> visited(H, vector<int>(W, -1));
+	queue<pair<int, int>> q;
+	visited[sx][sy] = 0;
+	q.push(make_pair(sx, sy));
+
+	while (!q.empty())
+	{
+		int x, y;
+		tie(x, y) = q.front(); q.pop();
+
+		for (int k = 0; k < 4; k++)
+		{
+			int nx = x + dx[k];
+			int ny = y + dy[k];
+			while (0 <= nx && nx < H && 0 <= ny && ny < W)
+			{
+				if (map[nx][ny] == '*') break;
+				if (visited[nx][ny] == -1)
+				{
+					visited[nx][ny] = visited[x][y] + 1;
+					q.push(make_pair(nx, ny));
+				}
+				nx += dx[k];
+				ny += dy[k];
+			}
+		}
+	}
+	cout << visited[ex][ey] - 1 << endl;
 }
-
