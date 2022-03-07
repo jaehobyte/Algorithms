b22adb3499d1cd481a09b3a07770a017af4bd9e8
jaehomersimpson <p5564989@gmail.com>
2022-03-07 17:42:54 +0900

    Greedy - coins
    Greedy - coins

---
+++ baekjoon_11047/baekjoon_11047.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int N, M;
+	cin >> N >> M;
+
+	vector<int> a(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> a[i];
+	}
+	int ans = 0;
+
+	for (int i = N - 1; i >= 0; i--)
+	{
+		ans += M / a[i];
+		M %= a[i];
+	}
+
+	cout << ans << endl;
+	return 0;
+
+}
