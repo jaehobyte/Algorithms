57bdd20cfd50f27795107937ce062fe5bb6f4ced
jaehomersimpson <p5564989@gmail.com>
2022-03-18 17:04:19 +0900

    Greedy - ATM
    Greedy

---
+++ baekjoon_11399/baekjoon_11399.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> list(N);
+
+    for (int i = 0; i < N; i++)
+       cin >> list[i];
+    sort(list.begin(), list.end());
+    
+    int sum = 0;
+    int ans = 0;
+
+    for (int j = 0; j < N; j++)
+    {
+        sum += list[j];
+        ans += sum;
+    }
+
+    cout << ans << endl;
+    return 0;
+}
