ef687d5e8e7f4cab3a330624ef9ed7bdd4e0ce89
jaehomersimpson <p5564989@gmail.com>
2022-03-20 13:17:12 +0900

    Greedy - numerical progression
    Greedy - numerical progression

---
+++ baekjoon_12738/baekjoon_12738.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> num;
+	for (int i = 0; i < n; i++)
+	{
+		int t;
+		cin >> t;
+		
+		if(num.empty()||num.back()<t)
+		{
+			num.push_back(t);
+		}
+		else 
+		{
+			auto temp = lower_bound(num.begin(), num.end(), t);
+			*temp = t;
+		}
+	}
+	cout << num.size() << endl;
+}
+
