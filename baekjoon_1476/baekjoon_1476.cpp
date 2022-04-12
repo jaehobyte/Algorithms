8312b4178990eb9dd10fed7de310a7df1201599e
jaehomersimpson <p5564989@gmail.com>
2022-04-13 00:23:16 +0900

    Brute force - date calculation
    Brute force - date calculation

---
+++ baekjoon_1476/baekjoon_1476.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int e, s, m;
+    cin >> e >> s >> m;
+    cout << (6916 * e + 4845 * s + 4200 * m - 1) % (15 * 28 * 19) + 1 << '\n';
+    return 0;
+}
