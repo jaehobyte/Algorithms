10a38bbaa2f43be10ac776674140c9f2e45d8d2c
jaehomersimpson <p5564989@gmail.com>
2022-05-07 17:41:13 +0900

    Dynamic Programming - Change to 1
    Dynamic Programming - Change to 1
    Bottom-up

---
+++ baekjoon_1463/baekjoon_1463(2).cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int map[1000001];
+
+int main() {
+
+    int n;
+    cin >> n;
+    map[1] = 0;
+
+    for (int i = 2; i <= n; i++) {
+        map[i] = map[i - 1] + 1;
+
+        if (i % 2 == 0 && map[i] > map[i / 2] + 1) {
+            map[i] = map[i / 2] + 1;
+        }
+
+        if (i % 3 == 0 && map[i] > map[i / 3] + 1) {
+            map[i] = map[i / 3] + 1;
+        }
+
+    }
+
+    cout << map[n] << '\n';
+    return 0;
+}
