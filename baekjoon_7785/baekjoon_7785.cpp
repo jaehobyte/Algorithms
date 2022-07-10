43c88ae8c0e312ebb25f540ad0cbf2dcbd4809ac
jaehomersimpson <p5564989@gmail.com>
2022-07-10 14:34:12 +0900

    Binary Tree - person in the company
    Binary Tree - person in the company

---
+++ baekjoon_7785/baekjoon_7785.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <set>
+
+using namespace std;
+
+/* 정렬된 이진트리를 확인하기 위한 문제
+c++에서는 set, map을 사용하면 된다. 정렬된 상태가 필요없다면 unordered_set을 사용하면 됨 
+rbegin을 사용하는 이유는 역순으로 출력 전위순회, 후위순회에 관련됨 */
+
+int main() {
+	int n;
+	cin >> n;
+	set<string> data;
+
+	while (n--)
+	{
+		string name, what;
+		cin >> name >> what;
+
+		if (what == "enter") {
+			data.insert(name);
+		}
+		else {
+			data.erase(data.find(name));
+		}
+	}
+
+	for (auto i = data.rbegin(); i != data.rend(); i++)
+	{
+		cout << *i << endl;
+	}
+	return 0;
+}
